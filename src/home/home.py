dfaf4d4b4ae9f511f3c25420ed74ea4cd096048c
Jacob Richard <JacobDanielRichard@yahoo.com>
2020-11-04 12:07:06 -0700

    Move Home assets to its respective static folder

--- src/home/home.py
+++ src/home/home.py
@@ -3,7 +3,8 @@ from flask import Blueprint, render_template
 home_bp = Blueprint(
     'home_bp', __name__,
     template_folder='templates',
-    static_folder='static'
+    static_folder='static',
+    static_url_path='/home/static'
 )
 
 
